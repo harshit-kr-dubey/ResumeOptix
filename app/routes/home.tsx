85bb342d1d254274ff1f7140b272ad7f37699e9b
harshit <harshit10561@gmail.com>
2025-07-23 22:54:33 +0530

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -10,7 +10,7 @@ import{Link, useNavigate} from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "HR Learning Centre" },
+    { title: "HR Learning Center" },
     { name: "description", content: "Smart Feedback for your dream job!" },
   ];
 }
